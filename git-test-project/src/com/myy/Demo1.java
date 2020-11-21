d26599e40ef57156e018b2b7cc6133ed369e15b7
myy <1136997460@qq.com>
2020-11-21 15:26:31 +0800

    + do2

--- git-test-project/src/com/myy/Demo1.java
+++ git-test-project/src/com/myy/Demo1.java
@@ -4,7 +4,10 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("main");
     }
-    private void do1(){
+    private static void do1(){
         System.out.println("do1");
+    }
+    private static void do2(){
+        System.out.println("do2");
     }
 }
